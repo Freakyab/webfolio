fa2eeb73aac0ffd716926134673572dd42dff301
Freakyab <aryanbhisikar1@gmail.com>
2024-06-15 13:40:35 +0530

    chore: Update page styles for About and Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useMemo, useState, useEffect } from "react";
+import Image from "next/image";
 import PageList from "./components/pageList";
 
 const TypeEffect = () => {
@@ -50,18 +51,18 @@ const TypeEffect = () => {
   }, [text, arrayIndex, iter, array]);
 
   return (
-    <div
-      className="mt-[50%] sm:mt-0 sm:grid grid-cols-1 sm:grid-cols-3 gap-1 sm:ml-0 h-[calc(100vh-190px)] font-medium "
-      style={{ textShadow: "0px 0px 10px rgba(255,255,255,0.5)" }}
-    >
-      <div className="col-span-1 sm:col-span-1 grid place-items-center">
-        <h1 className="text-3xl sm:text-6xl">Hello,</h1>
-      </div>
-      <div className="col-span-1 mr-auto sm:col-span-2 grid place-items-center">
-        <p className="text-xl sm:text-4xl font-light">
-          I am {"<"} <span
-            className="active-text font-extrabold "
-            style={{ textShadow: "0px 0px 5px rgba(0,0,0,0.2)" }}
+    <div className="font-medium text-white" style={{ textShadow: "0px 0px 10px rgba(255,255,255,0.5)" }}>
+      <div className="">
+        <p
+          className="text-xl sm:text-4xl font-light"
+          style={{
+            fontFamily: "monospace",
+          }}
+        >
+          I am {"<"}{" "}
+          <span
+            className="active-text font-extrabold"
+            style={{ textShadow: "0px 0px 5px rgba(255,255,255,0.2)" }}
           >
             {text}
           </span>
@@ -73,14 +74,66 @@ const TypeEffect = () => {
 };
 
 const Home = () => {
+  const [image, setImage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://imageuploaderfreakyab.vercel.app/api/getImage/666d2b4fe01f66c2d3117195",
+          {
+            method: "GET",
+            mode: "cors",
+            headers: {
+              Accept: "*/*",
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.imageUrl) {
+              setImage(data.imageUrl);
+            }
+          });
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <main className="body_color pl-12 md:pl-0  w-[100vw] calc_height text-white">
+    <main className="body_color pl-12 md:pl-0 w-full text-white">
       <PageList />
-      <div className="fixed h-full pl-10 mt-[10%] md:mt-0 ml-10 solidText font-bold text-[5rem] md:text-[10rem] opacity-10 z-2">
-        BUILD <span className=" ">WHAT</span> <br />
-        YOU <code className="outLine ">LOVE</code>
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center p-8 md:p-16 gap-8">
+        <div className="flex flex-col items-start justify-center gap-9 relative z-10 rounded-lg py-16 px-8 active-border-all transition-all duration-500 shadow-lg shadow-black hover:bg-black hover:text-white">
+          <h1 className="text-5xl font-bold animate-pulse">Hi,</h1>
+          <TypeEffect />
+          <p className="text-xl sm:text-3xl font-light italic bg-white text-black rounded-lg p-3 shadow-lg shadow-black">
+            {`<> "Build what you love" </>`}
+          </p>
+          <button 
+            className="bg-white text-black  italic text-xl py-2 px-4 rounded-md capitalize transition-transform transform hover:scale-105 shadow-lg hover:shadow-xl"
+            onClick={() => window.location.href = "/contact"}
+          >
+            Contact me
+          </button>
+        </div>
+        <div className="flex justify-center relative">
+          <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 p-1 rounded-full transition-transform">
+            {image && (
+              <Image
+                src={image || "/default-profile.png"} // Fallback image if `image` is empty
+                alt="profile"
+                width={300}
+                height={300}
+                className="rounded-full filter grayscale shadow-lg hover:grayscale-0 transition-all duration-500"
+              />
+            )}
+          </div>
+        </div>
       </div>
-      <TypeEffect />
     </main>
   );
 };
